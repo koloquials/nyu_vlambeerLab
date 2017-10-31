2c0916fa0b6265fbcd3e2b274a0932243e440242
Unknown <rg3112@nyu.edu>
2017-10-31 00:35:00 -0400

    implemented the pseudocode, replaced floortile with three random tiles (moon, star, sun), added replay button

---
+++ Assets/scripts/ResetManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetManager : MonoBehaviour {
+	//public bool canRestart = false;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R) && Pathmaker.canRestart == true) {
+			print ("space key was pressed");
+			Pathmaker.totalTileCount = 0;
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+}
